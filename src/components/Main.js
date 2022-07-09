d63803a2da24fd4f4de72adc78d068c58bc1535f
jkarnage <jakarnage@gmail.com>
2022-07-08 20:24:30 -0400

    Added component files and other scaffolding to the project.

---
+++ src/components/Main.js
@@ -0,0 +1,7 @@
+export default function Main() {
+  return (
+    <main>
+      <h1>This is the main element</h1>
+    </main>
+  );
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,3 @@
+export default function Navbar() {
+  return <nav>This is the nav element</nav>;
+}
