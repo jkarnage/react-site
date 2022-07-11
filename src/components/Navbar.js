01939e260832a173a98647e331ceb8dea27e7f13
jkarnage <jakarnage@gmail.com>
2022-07-11 13:42:51 -0400

    Added images, and updated Main and Nav compoments

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,11 @@
+import reactIcon from "./images/react-icon-small.png";
+
 export default function Navbar() {
-  return <nav>This is the nav element</nav>;
+  return (
+    <nav>
+      <img src={reactIcon} alt="Small React Logo" className="nav--icon" />
+      <h3 className="nav--logo_text">ReactFacts</h3>
+      <h4 className="nav--title">React Course - Project 1</h4>
+    </nav>
+  );
 }
